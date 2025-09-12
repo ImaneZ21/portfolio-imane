3c1fe386b12ae757ad1062020a80af3c544d57df
ImaneZ21 <zerdag.imane@gmail.com>
2025-09-12 12:08:04 +0200

    add about section

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -3,7 +3,27 @@ import "./About.scss";
 function About() {
   return (
     <div className="about d-flex">
-      <p>Bonjour Je m'appelle Imane</p>
+      <div className="containeur">
+        <h1>A propos</h1>
+        <p className="about-paragraphs">
+          Primi igitur omnium statuuntur Epigonus et Eusebius ob nominum
+          gentilitatem oppressi. praediximus enim Montium sub ipso vivendi
+          termino his vocabulis appellatos fabricarum culpasse tribunos ut
+          adminicula futurae molitioni pollicitos. Tantum autem cuique
+          tribuendum, primum quantum ipse efficere possis, deinde etiam quantum
+          ille quem diligas atque adiuves, sustinere. Non enim neque tu possis,
+          quamvis excellas, omnes tuos ad honores amplissimos perducere, ut
+          Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non
+          potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est
+          tamen, quid ille possit sustinere. Fieri, inquam, Triari, nullo pacto
+          potest, ut non dicas, quid non probes eius, a quo dissentias. quid
+          enim me prohiberet Epicureum esse, si probarem, quae ille diceret? cum
+          praesertim illa perdiscere ludus esset. Quam ob rem dissentientium
+          inter se reprehensiones non sunt vituperandae, maledicta, contumeliae,
+          tum iracundiae, contentiones concertationesque in disputando
+          pertinaces indignae philosophia mihi videri solent.
+        </p>
+      </div>
     </div>
   );
 }
