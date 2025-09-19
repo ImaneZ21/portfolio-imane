fe3da731694663b5c6f76c01b673dd9439f87b77
ImaneZ21 <zerdag.imane@gmail.com>
2025-09-19 11:17:11 +0200

    change color + add title

---
+++ components/Title/Title.jsx
@@ -0,0 +1,11 @@
+import "./Title.scss";
+
+function Title() {
+  return (
+    <div className="title d-flex">
+        <h1>DEVELOPPEUSE WEB FULL STACK</h1>
+    </div>
+  );
+}
+
+export default Title;
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header/Header.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import About from "../components/About/About.jsx";
-import Contact from "../components/Contact/Contact.jsx"
+import Contact from "../components/Contact/Contact.jsx";
+import Title from "../components/Title/Title.jsx"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Header />
       </header>
       <main>
+        <Title />
         <About />
         <Contact />
         {/* <Projects />
