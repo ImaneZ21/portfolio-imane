7eef5615a31903d63a1540096d0322c49ef578aa
ImaneZ21 <zerdag.imane@gmail.com>
2025-09-19 11:53:33 +0200

    picture for title

--- components/Title/Title.jsx
+++ components/Title/Title.jsx
@@ -1,9 +1,11 @@
 import "./Title.scss";
+import image from "../../src/assets/image.png";
 
 function Title() {
   return (
     <div className="title d-flex">
         <h1>DEVELOPPEUSE WEB FULL STACK</h1>
+        <img src={image} alt="Illustration développeuse web"></img>
     </div>
   );
 }
